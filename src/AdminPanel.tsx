784bf8b1fa845540a3c5dea22171e5e09dbba99c
Pratik Anand <grufelous@gmail.com>
2021-05-23 18:14:41 +0530

    Added SASS module

--- src/AdminPanel.tsx
+++ src/AdminPanel.tsx
@@ -3,32 +3,30 @@ import { Product } from './types';
 
 //review: add types for storeItems, addItem, deleteItem
 const AdminPanel = ({ storeItems, addItem, deleteItem }) => {
-    //review: no need to name variable as newName. new keyword is rendundant here. we can keep variable name as `name`, `make`, `price`
-    const [newName, setNewName] = useState('');
-    const [newMake, setNewMake] = useState('');
-    const [newPrice, setNewPrice] = useState('');
-    const [newDescription, setNewDescription] = useState('');
+    //review: no need to name variable as name. new keyword is rendundant here. we can keep variable name as `name`, `make`, `price`
+    const [name, setName] = useState('');
+    const [make, setMake] = useState('');
+    const [price, setPrice] = useState('');
+    const [description, setDescription] = useState('');
     //review: wrap handleNewItem with useCallback
     const handleNewItem = (e: { preventDefault: () => void; }) => {
         e.preventDefault();
         const newItem = {
-            'name': newName,
-            'make': newMake,
-            'price': (Number.parseFloat(newPrice)) ? Number.parseFloat(newPrice) : 10, //review: why keeping default value as 10?
-            'description': newDescription,
+            'name': name,
+            'make': make,
+            'price': (Number.parseFloat(price)) ? Number.parseFloat(price) : 10, //review: why keeping default value as 10?
+            'description': description,
             'id': '42',
         };
         addItem(newItem);
     }
     const renderStoreItems = (): JSX.Element[] => {
-        console.log(`Type: ${typeof storeItems}`)
-        console.log(storeItems)
         //review: if you have a unique identifier in our object always use that as your key. index should be last resort. Use Id as key here
         return storeItems.map((storeItem: Product) => (
             <>
                 <li 
                  key={storeItem.id}>
-                <span key={storeItem.id}>{storeItem.name}</span>
+                <span key={'item_' + storeItem.id}>{storeItem.name}</span>
                     <button
                      onClick={() => deleteItem(storeItem.id)}
                      key={'delBtn' + storeItem.id}>
@@ -39,17 +37,17 @@ const AdminPanel = ({ storeItems, addItem, deleteItem }) => {
         ))
     }
     
-    //review: rename to renderAddItemForm. addItemForm looks like a name for an action.
+    //review: rename to renderAddItemForm. addItemForm looks like a name for an action. ✅
     //review: we can also take this out as a separate component and wrap with React.memo.
-    //review: take out (e) => setNewName(e.target.value) in a function `onNameChange` and use `useCallback` to avoid creating new function on each render
-    //review: keep price input type as number and not text.
-    const addItemForm = (): JSX.Element => {
+    //review: take out (e) => setName(e.target.value) in a function `onNameChange` and use `useCallback` to avoid creating new function on each render
+    //review: keep price input type as number and not text. ✅
+    const renderAddItemForm = (): JSX.Element => {
         return (<>
             <form onSubmit={handleNewItem}>
-                <input type="text" placeholder="Name" value={newName} onChange={(e) => setNewName(e.target.value)}/>
-                <input type="text" placeholder="Make" value={newMake} onChange={(e) => setNewMake(e.target.value)} />
-                <input type="number" placeholder="Price" value={newPrice} onChange={(e) => setNewPrice(e.target.value)} />
-                <input type="text" placeholder="Description" value={newDescription} onChange={(e) => setNewDescription(e.target.value)} />
+                <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
+                <input type="text" placeholder="Make" value={make} onChange={(e) => setMake(e.target.value)} />
+                <input type="number" placeholder="Price" value={price} onChange={(e) => setPrice(e.target.value)} />
+                <input type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
                 <input type="submit" value="Add"/>
             </form>
         </>)
@@ -58,7 +56,7 @@ const AdminPanel = ({ storeItems, addItem, deleteItem }) => {
         <>
             <h1>Admin Panel</h1>
             <h3>Add new items: </h3>
-            {addItemForm()}
+            {renderAddItemForm()}
             <h3>All items in store: </h3>
             {renderStoreItems()}
         </>
